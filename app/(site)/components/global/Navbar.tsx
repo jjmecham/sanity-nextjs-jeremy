9510848476068041ec70cb6ca6785e9c6aac2a54
Jeremy <98417361+jjmecham@users.noreply.github.com>
2024-08-28 17:14:07 -0400

    move themeswitch

--- app/(site)/components/global/Navbar.tsx
+++ app/(site)/components/global/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "../../icons/logo.png";
-import ThemeSwitch from "../ThemeSwitch";
+import ThemeSwitch from "./ThemeSwitch";
 import { useState } from 'react'
 import { BiMenuAltRight } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'

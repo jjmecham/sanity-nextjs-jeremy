31e1950bd6471ef15fb2ec75e52ed591d1d2d746
Jeremy <98417361+jjmecham@users.noreply.github.com>
2024-08-28 11:10:21 -0400

    ignore masonary

--- app/(site)/components/Masonary.tsx
+++ app/(site)/components/Masonary.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import { Masonry } from "react-plock";
+// import { Masonry } from "react-plock";
 
-const ImagesMasonry = () => {
-  const items = [...imageUrls];
+// const ImagesMasonry = () => {
+//   const items = [...imageUrls];
 
-  return (
-    <Masonry
-      items={items}
-      config={{
-        columns: [1, 2, 3],
-        gap: [24, 12, 6],
-        media: [640, 768, 1024],
-      }}
-      render={(item, idx) => (
-        <img key={idx} src={item} style={{ width: "100%", height: "auto" }} alt=""/>
-      )}
-    />
-  );
-};
+//   return (
+//     <Masonry
+//       items={items}
+//       config={{
+//         columns: [1, 2, 3],
+//         gap: [24, 12, 6],
+//         media: [640, 768, 1024],
+//       }}
+//       render={(item, idx) => (
+//         <img key={idx} src={item} style={{ width: "100%", height: "auto" }} alt=""/>
+//       )}
+//     />
+//   );
+// };
 
 

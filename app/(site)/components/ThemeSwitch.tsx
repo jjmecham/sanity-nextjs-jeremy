b2e581f88278c645178a4ff4fdacce851d0add5d
Jeremy <98417361+jjmecham@users.noreply.github.com>
2024-08-28 12:57:36 -0400

    change text size

--- app/(site)/components/ThemeSwitch.tsx
+++ app/(site)/components/ThemeSwitch.tsx
@@ -3,7 +3,7 @@
 import { FiSun, FiMoon } from "react-icons/fi"
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
-import Image from "next/image"
+// import Image from "next/image"
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
@@ -17,22 +17,22 @@ export default function ThemeSwitch() {
 
   if (resolvedTheme === 'dim') {
     // return <div className="mt-2 cursor-pointer text-primary"><FiSun onClick={() => setTheme('retro')}/></div>
-    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('retro')}>Light</span></div>
+    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('retro')} className="text-xs">Light</span></div>
   }
 
   if (resolvedTheme === 'retro') {
     // return <div className="mt-2 cursor-pointer text-primary"><FiMoon onClick={() => setTheme('business')} /></div>
-    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('business')}>Dark</span></div>
+    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('business')} className="text-xs">Dark</span></div>
   }
 
   if (resolvedTheme === 'business') {
     // return <div className="mt-2 cursor-pointer text-primary"><FiSun onClick={() => setTheme('emerald')} /></div>
-    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('emerald')}>Light</span></div>
+    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('emerald')} className="text-xs">Light</span></div>
   }
   
   else if (resolvedTheme === 'emerald'){
     // return <div className="mt-2 cursor-pointer text-primary"><FiMoon onClick={() => setTheme('dim')} /></div>
-    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('dim')}>Dark</span></div>
+    return <div className="mt-2 cursor-pointer text-primary"><span onClick={() => setTheme('dim')} className="text-xs">Dark</span></div>
   }
 
 }
